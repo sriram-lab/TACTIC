ad8890de2cdd20fd38ec1fc13207e075e791fe53
chechung <chechung@umich.edu>
2024-12-02 15:05:03 -0500

    Adding new files after MSB revision.

---
+++ TACTIC.py
@@ -0,0 +1,16 @@
+# Import dependencies
+from indigopy.core import featurize
+
+# Define TACTIC function
+def tactic_featurize(drug_response, drug_interactions, orthologs, remove_duplicates): 
+    # Check inputs
+    drug_response
+
+    # Determine ML features
+
+
+    # Harmonize data
+
+
+    # Return output
+    return df
